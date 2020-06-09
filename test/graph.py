f3f1966fe2bd510b4de121ba23d700f6423ab3cc
Jorge Salazar Cruz <cruzjorgesalazar@gmail.com>
2020-06-08 20:11:19 -0500

    Se añadieron las gradicas con los datos obtenidos

--- test/graph.py
+++ test/graph.py
@@ -4,31 +4,28 @@ import matplotlib.patches as mpatches
 xs1 = [100, 1000, 10000, 100000]
 xs2 = [100, 1000, 10000, 100000, 1000000, 5000000, 10000000]
 
+name = "Random order"
+ys4 = [0.0, 0.0, 4.33, 57.33, 725.33, 4250.67, 9015.0]
+ys5 = [0.0, 0.0, 5.0, 66.33, 889.0, 4887.33, 10366.67]
+ys6 = [0.0, 0.0, 4.0, 58.0, 852.0, 5909.0, 13176.67]
+ys7 = [0.0, 0.0, 2.0, 32.33, 432.33, 2221.33, 4507.0]
 
-name = "Sorted asc with 20% adj swaps"
-ys1 = [0.0, 3.0, 297.8, 59400.8]
-ys2 = [0.0, 0.0, 0.0, 6.8]
-ys3 = [0.0, 7.0, 755.6, 74738.4]
-ys4 = [0.0, 0.0, 3.0, 33.8]
-ys5 = [0.0, 0.0, 4.2, 62.6]
-ys6 = [0.0, 0.0, 4.0, 51.0]
-ys7 = [0.0, 0.0, 1.0, 18.4]
 
 fig = plt.figure()
 fig.suptitle(name, fontsize=14, fontweight='bold')
 
-plt.plot(xs1, ys1, 'red')
-plt.plot(xs1, ys2, 'blue')
-plt.plot(xs1, ys3, 'orange')
-plt.plot(xs1, ys4, 'purple')
-plt.plot(xs1, ys5, 'yellow')
-plt.plot(xs1, ys6, 'green')
-plt.plot(xs1, ys7, 'black')
+# plt.plot(xs1, ys1, 'red')
+# plt.plot(xs1, ys2, 'blue')
+# plt.plot(xs1, ys3, 'orange')
+# plt.plot(xs1, ys4, 'purple')
+# plt.plot(xs1, ys5, 'yellow')
+# plt.plot(xs1, ys6, 'green')
+# plt.plot(xs1, ys7, 'black')
 
-# plt.plot(xs2, ys4, 'purple')
-# plt.plot(xs2, ys5, 'yellow')
-# plt.plot(xs2, ys6, 'green')
-# plt.plot(xs2, ys7, 'black')
+plt.plot(xs2, ys4, 'purple')
+plt.plot(xs2, ys5, 'yellow')
+plt.plot(xs2, ys6, 'green')
+plt.plot(xs2, ys7, 'black')
 # plt.axis([0, 10000000, 0, 12000])
 
 
@@ -40,10 +37,10 @@ yellow_patch = mpatches.Patch(color='yellow')
 green_patch = mpatches.Patch(color='green')
 black_patch = mpatches.Patch(color='black')
 
-plt.legend([red_patch, blue_patch, orange_patch, purple_patch, yellow_patch, green_patch, black_patch],
-           ["bubbleSort", "insertionSort", "selectionSort", "mergeSort", "quickSort", "heapSort", "std::sort"])
-# plt.legend([purple_patch, yellow_patch, green_patch, black_patch],
-#            ["mergeSort", "quickSort", "heapSort", "std::sort"])
+# plt.legend([red_patch, blue_patch, orange_patch, purple_patch, yellow_patch, green_patch, black_patch],
+#            ["bubbleSort", "insertionSort", "selectionSort", "mergeSort", "quickSort", "heapSort", "std::sort"])
+plt.legend([purple_patch, yellow_patch, green_patch, black_patch],
+           ["mergeSort", "quickSort", "heapSort", "std::sort"])
 
 plt.xlabel("input size")
 plt.ylabel("Average time (in milliseconds)")
